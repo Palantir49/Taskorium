bedb30800a79b6d77598b4f7fb1aef702ea1fcc1
palantir49 <pupkin.dus@yandex.ru>
2026-01-08 23:41:32 +0300

    [feature] Add global exception handler

--- src/backend/Services/Task/TaskService.Api/Program.cs
+++ src/backend/Services/Task/TaskService.Api/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.OpenApi;
 using Scalar.AspNetCore;
+using TaskService.Api.Handlers;
 using TaskService.Application.Extensions;
 using TaskService.Infrastructure.Extensions;
 
@@ -10,6 +11,9 @@ builder.Configuration
     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true)
     .AddEnvironmentVariables()
     .Build();
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -29,8 +33,7 @@ builder.Services.AddOpenApi(options =>
 
         document.Info.License = new OpenApiLicense
         {
-            Name = "MIT License",
-            Url = new Uri("https://opensource.org/licenses/MIT")
+            Name = "MIT License", Url = new Uri("https://opensource.org/licenses/MIT")
         };
         return Task.CompletedTask;
     });
@@ -42,8 +45,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp", policy =>
     {
         policy.WithOrigins("http://localhost:5000")
-              .AllowAnyHeader()
-              .AllowAnyMethod();
+            .AllowAnyHeader()
+            .AllowAnyMethod();
     });
 });
 
@@ -63,6 +66,7 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 
+app.UseExceptionHandler();
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
