700a4bad7b585f89f90a7be02f208d7398c328f8
palantir49 <pupkin.dus@yandex.ru>
2026-01-08 17:41:49 +0300

    [fix] format

--- src/backend/Services/Task/TaskService.Application/Mediator/Dispatcher.cs
+++ src/backend/Services/Task/TaskService.Application/Mediator/Dispatcher.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace TaskService.Application.Mediator;
--- src/backend/Services/Task/TaskService.Application/Mediator/IRequest.cs
+++ src/backend/Services/Task/TaskService.Application/Mediator/IRequest.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace TaskService.Application.Mediator;
 
