c91a5d1a9caea74d3ac93f88af8764992c629bb0
palantir49 <pupkin.dus@yandex.ru>
2026-01-29 17:41:54 +0300

    [feature] Add methods to repository

--- src/backend/Services/Task/TaskService.Domain/Repositories/IRepositoryBase.cs
+++ src/backend/Services/Task/TaskService.Domain/Repositories/IRepositoryBase.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Text;
+﻿using System.Linq.Expressions;
 
 namespace TaskService.Domain.Repositories;
 
@@ -12,9 +9,12 @@ public interface IRepositoryBase<T>
     Task UpdateAsync(T entity, CancellationToken ct = default);
     Task DeleteAsync(T entity, CancellationToken ct = default);
 
-    //Task<T?> GetByConditionAsync(
-    //    Expression<Func<T, bool>> predicate,
-    //    CancellationToken ct = default);
+    Task<List<T>?> GetListByConditionAsync(Expression<Func<T, bool>>? predicate = null,
+        Func<IQueryable<T>, IQueryable<T>>? include = null, CancellationToken ct = default);
+
+    Task<T?> GetByConditionAsync(Expression<Func<T, bool>>? predicate = null,
+        Func<IQueryable<T>, IQueryable<T>>? include = null, CancellationToken ct = default);
+
 
     //Task<List<T>> GetAllAsync(
     //    CancellationToken ct = default);
