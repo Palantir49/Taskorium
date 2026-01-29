5dc1911bfc0a58fcad3b9261524413610ca73bd7
palantir49 <pupkin.dus@yandex.ru>
2026-01-29 17:37:26 +0300

    [feature] Add fullName field

--- src/backend/Services/Task/TaskService.Domain/Entities/User.cs
+++ src/backend/Services/Task/TaskService.Domain/Entities/User.cs
@@ -4,28 +4,30 @@ namespace TaskService.Domain.Entities;
 
 public class User
 {
-    public Guid Id { get; }
-    public Guid KeycloakId { get; private set; }
-    public EmailAdress Email { get; private set; }
-    public UserName Username { get; private set; }
-    public DateTimeOffset CreatedDate { get; }
-    protected User()
-    {
-        Email = null!;
-        Username = null!;
-    }
-    private User(Guid id, Guid keycloakId, UserName userName, EmailAdress email, DateTimeOffset createdAt)
+    private User(Guid id, Guid keycloakId, UserName userName, EmailAdress email, DateTimeOffset createdAt,
+        string fullName)
     {
         Id = id;
         Username = userName;
         KeycloakId = keycloakId;
         Email = email;
         CreatedDate = createdAt;
+        FullName = fullName;
     }
-    public static User Create(Guid keycloakId, UserName userName, EmailAdress email)
+
+    public Guid Id { get; }
+    public Guid KeycloakId { get; private set; }
+    public EmailAdress Email { get; private set; }
+    public UserName Username { get; private set; }
+    public DateTimeOffset CreatedDate { get; }
+
+    public string FullName { get; }
+
+    public static User Create(Guid keycloakId, UserName userName, EmailAdress email, string fullName)
     {
-        return new User(Guid.CreateVersion7(), keycloakId, userName, email, DateTimeOffset.Now);
+        return new User(Guid.CreateVersion7(), keycloakId, userName, email, DateTimeOffset.Now, fullName);
     }
+
     public void UpdateEmail(EmailAdress newEmail)
     {
         Email = newEmail;
