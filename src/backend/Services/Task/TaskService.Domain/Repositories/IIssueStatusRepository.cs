1708cc97d7fefc299e22f6cd25fcbf042cdcbe02
jlamma <jlampjlamma@gmail.com>
2026-01-31 13:16:09 +0300

    [feature] add issue status in ef

---
+++ src/backend/Services/Task/TaskService.Domain/Repositories/IIssueStatusRepository.cs
@@ -0,0 +1,9 @@
+﻿using TaskService.Domain.Entities;
+
+namespace TaskService.Domain.Repositories
+{
+    public interface IIssueStatusRepository : IRepositoryBase<IssueStatus>
+    {
+        Task<List<IssueStatus>> GetByProjectIdAsync(Guid projectId, CancellationToken ct = default);
+    }
+}
