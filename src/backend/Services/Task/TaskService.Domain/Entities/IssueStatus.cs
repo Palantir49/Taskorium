e1859d23e6d098429c4d23a53128fcad6d06b786
jlamma <jlampjlamma@gmail.com>
2026-01-31 13:16:09 +0300

    [feature] add issue status in ef

--- src/backend/Services/Task/TaskService.Domain/Entities/IssueStatus.cs
+++ src/backend/Services/Task/TaskService.Domain/Entities/IssueStatus.cs
@@ -5,27 +5,33 @@ namespace TaskService.Domain.Entities;
 
 public class IssueStatus : BaseEntities
 {
+    public Guid ProjectId { get; private set; }
     public IssueStatusType Type { get; private set; }
     public int Position { get; private set; }
     //TODO: уточнить, будем ли что-то подобное хранить. Если да - сделать VO под HEX
-    public string Color { get; private set; }
+    public string? Color { get; private set; }
 
-    protected IssueStatus()
-    {
-        Color = "#0000FF";
-    }
+    protected IssueStatus() { }
 
-    private IssueStatus(Guid id, string name, IssueStatusType type, int position, string color) : base(id, name)
+    private IssueStatus(Guid id, string name, IssueStatusType type, int position, string color, Guid projectId) : base(id, name)
     {
         Type = type;
         Position = position;
         Color = color;
+        ProjectId = projectId;
     }
 
-    public static IssueStatus Create(Guid id, string name, string type, int position, string color)
+    public static IssueStatus Create(string name, string type, int position, string color, Guid projectId)
     {
-        return new IssueStatus(id: id, name: name, type: Enum.Parse<IssueStatusType>(type), position: position, color: color);
+        return new IssueStatus(
+            id: Guid.CreateVersion7(), 
+            name: name, 
+            type: Enum.Parse<IssueStatusType>(type), 
+            position: position, 
+            color: color, 
+            projectId: projectId);
     }
+    public void UpdateType(IssueStatusType type) => Type = type;
 
     public void UpdatePosition(int position) => Position = position;
 
---
+++ src/backend/Services/Task/TaskService.Infrastructure/Configurations/IssueStatusConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskService.Domain.Entities;
+
+namespace TaskService.Infrastructure.Configurations;
+
+internal class IssueStatusConfiguration : IEntityTypeConfiguration<IssueStatus>
+{
+    public void Configure(EntityTypeBuilder<IssueStatus> builder)
+    {
+        builder.HasKey(t => t.Id);
+
+        builder.Property(t => t.Id).ValueGeneratedNever();
+        builder.Property(t => t.ProjectId).IsRequired();
+        builder.Property(t=>t.Type).IsRequired();
+        builder.Property(t=>t.Position).IsRequired();
+        //TODO: если будет нужен, то сделать обязательным или в сущности добавить дефолт
+        builder.Property(t=>t.Color);
+    }
+}
