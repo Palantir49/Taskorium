138492d5b54b62e9981f46cce67541cc53e6a34f
jlamma <jlampjlamma@gmail.com>
2026-01-31 17:25:20 +0300

    [feature] added handler to get issues get by project id

---
+++ src/backend/Services/Task/TaskService.Application/Features/Issues/Command/IssueGetByProjectIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using TaskService.Application.Mediator;
+using TaskService.Contracts.Issue.Responses;
+
+namespace TaskService.Application.Features.Issues.Command
+{
+    public record class IssueGetByProjectIdQuery (Guid projectId) : IQuery <IEnumerable<IssueResponse>>;
+}
