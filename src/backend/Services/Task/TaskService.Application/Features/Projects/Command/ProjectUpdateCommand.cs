c4d8d3e3309d6ceb264963706e553feb51ea34d8
jlamma <jlampjlamma@gmail.com>
2026-01-31 21:51:29 +0300

    [feature] added project commands

---
+++ src/backend/Services/Task/TaskService.Application/Features/Projects/Command/ProjectUpdateCommand.cs
@@ -0,0 +1,10 @@
+﻿using TaskService.Application.Mediator;
+using TaskService.Contracts.Project.Responses;
+
+namespace TaskService.Application.Features.Projects.Command;
+
+public record ProjectUpdateCommand(
+    Guid id,
+    string Name,
+    string Description,
+    Guid WorkspaceId) : ICommand<ProjectResponse>;
