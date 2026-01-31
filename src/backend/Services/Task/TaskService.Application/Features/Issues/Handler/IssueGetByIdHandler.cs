c4a75bdd25bf7b06f1a0f719d78c847ad14ce400
jlamma <jlampjlamma@gmail.com>
2026-01-31 17:04:09 +0300

    [feature] added request to get a issue by Id

---
+++ src/backend/Services/Task/TaskService.Application/Features/Issues/Handler/IssueGetByIdHandler.cs
@@ -0,0 +1,20 @@
+﻿using TaskService.Application.Features.Issues.Command;
+using TaskService.Application.Features.Issues.Mapping;
+using TaskService.Application.Mediator;
+using TaskService.Contracts.Issue.Responses;
+using TaskService.Domain.Entities;
+using TaskService.Domain.Repositories;
+
+namespace TaskService.Application.Features.Issues.Handler;
+
+internal class IssueGetByIdHandler(IRepositoryWrapper wrapper) : IRequestHandler<IssueGetByIdQuery, IssueResponse>
+{
+    public async Task<IssueResponse> Handle(IssueGetByIdQuery request, CancellationToken cancellationToken = default)
+    {
+        Issue? issue = await wrapper.Issues.GetByIdAsync(request.id, cancellationToken);
+        if(issue == null)
+            throw new NullReferenceException($"задача с id: {request.id} не найдена");
+
+        return issue.ToResponce();
+    }
+}
