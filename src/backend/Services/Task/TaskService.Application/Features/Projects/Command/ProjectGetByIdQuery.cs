33b5db3caddf648f48e68bc4486a10cfce890777
jlamma <jlampjlamma@gmail.com>
2026-01-31 21:51:29 +0300

    [feature] added project commands

---
+++ src/backend/Services/Task/TaskService.Application/Features/Projects/Command/ProjectGetByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using TaskService.Application.Mediator;
+using TaskService.Contracts.Project.Responses;
+
+namespace TaskService.Application.Features.Projects.Command;
+
+public record class ProjectGetByIdQuery(Guid id) : IQuery<ProjectResponse>;
