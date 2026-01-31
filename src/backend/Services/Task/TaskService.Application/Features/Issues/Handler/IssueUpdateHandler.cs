1d01dca86638ac32e0451fb415f7329f8f686e55
jlamma <jlampjlamma@gmail.com>
2026-01-31 23:58:02 +0300

    [codecleanup]

--- src/backend/Services/Task/TaskService.Application/Features/Issues/Handler/IssueUpdateHandler.cs
+++ src/backend/Services/Task/TaskService.Application/Features/Issues/Handler/IssueUpdateHandler.cs
@@ -11,23 +11,16 @@ namespace TaskService.Application.Features.Issues.Handler
     {
         public async Task<IssueResponse> Handle(IssueUpdateCommand request, CancellationToken cancellationToken = default)
         {
-            Issue? issue = await wrapper.Issues.GetByIdAsync(request.id);
-
-            if (issue == null)
-                throw new NullReferenceException($"Задача с id: {request.id} не найдена");
+            Issue? issue = await wrapper.Issues.GetByIdAsync(request.id) ?? throw new NullReferenceException($"Задача с id: {request.id} не найдена");
 
             Project? project = await wrapper.Projects.GetByIdAsync(issue.ProjectId);
 
-            IssueStatus? status = await wrapper.IssueStatus.GetByIdAsync(request.IssueStatusId);
-
-            if (status == null)
+            IssueStatus? status = await wrapper.IssueStatus.GetByIdAsync(request.IssueStatusId) ??
                 throw new NullReferenceException($"Статус задачи с id: {request.IssueStatusId} не найдена");
 
             //TODO: проверить что можно менять на этот статус
 
-            IssueType? type = await wrapper.IssueType.GetByIdAsync(request.IssueTypeId);
-
-            if (type == null)
+            IssueType? type = await wrapper.IssueType.GetByIdAsync(request.IssueTypeId) ??
                 throw new NullReferenceException($"Тип задачи с id: {request.IssueTypeId} не найдена");
 
             //TODO: проверить что можно менять на этот тип
--- src/backend/Services/Task/TaskService.Application/Features/Projects/Handler/ProjectUpdateHandler.cs
+++ src/backend/Services/Task/TaskService.Application/Features/Projects/Handler/ProjectUpdateHandler.cs
@@ -7,12 +7,10 @@ using TaskService.Domain.Repositories;
 
 namespace TaskService.Application.Features.Projects.Handler;
 
-public  class ProjectUpdateHandler(IRepositoryWrapper wrapper) : IRequestHandler<ProjectUpdateCommand, ProjectResponse>
+public class ProjectUpdateHandler(IRepositoryWrapper wrapper) : IRequestHandler<ProjectUpdateCommand, ProjectResponse>
 {
     public async Task<ProjectResponse> Handle(ProjectUpdateCommand request, CancellationToken cancellationToken = default)
     {
-        //throw new NullReferenceException($"Задача с id: {request.id} не найдена");
-
         Project project = await wrapper.Projects.GetByIdAsync(request.id) ?? throw new NullReferenceException($"Проект с id: {request.id} не найдена");
 
         project.UpdateName(request.Name);
