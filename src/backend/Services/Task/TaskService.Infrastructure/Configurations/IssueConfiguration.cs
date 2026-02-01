0220e0a747a60b4c56ae375107043195b160dd8a
jlamma <jlampjlamma@gmail.com>
2026-02-01 16:58:54 +0300

    [feature] added migration for issue status and issue type

--- src/backend/Services/Task/TaskService.Infrastructure/Configurations/IssueConfiguration.cs
+++ src/backend/Services/Task/TaskService.Infrastructure/Configurations/IssueConfiguration.cs
@@ -3,42 +3,41 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using TaskService.Domain.Entities;
 using TaskService.Domain.ValueObjects;
 
-namespace TaskService.Infrastructure.Configurations
+namespace TaskService.Infrastructure.Configurations;
+
+internal class IssueConfiguration : IEntityTypeConfiguration<Issue>
 {
-    internal class IssueConfiguration : IEntityTypeConfiguration<Issue>
+    public void Configure(EntityTypeBuilder<Issue> builder)
     {
-        public void Configure(EntityTypeBuilder<Issue> builder)
-        {
-            builder.HasKey(t => t.Id);
+        builder.HasKey(t => t.Id);
 
-            builder.Property(t => t.Id).ValueGeneratedNever();
+        builder.Property(t => t.Id).ValueGeneratedNever();
 
-            builder.Property(t => t.Name).HasConversion(
-                name => name.Value,
-                value => new BaseEntityName(value))
-                .IsRequired().HasMaxLength(225);
+        builder.Property(t => t.Name).HasConversion(
+            name => name.Value,
+            value => new BaseEntityName(value))
+            .IsRequired().HasMaxLength(225);
 
-            builder.Property(t => t.Description).HasMaxLength(2000);
+        builder.Property(t => t.Description).HasMaxLength(2000);
 
-            builder.Property(t => t.CreatedDate).IsRequired();
-            builder.Property(t => t.UpdatedDate);
-            builder.Property(t => t.DueDate);
-            builder.Property(t => t.ResolvedDate);
+        builder.Property(t => t.CreatedDate).IsRequired();
+        builder.Property(t => t.UpdatedDate);
+        builder.Property(t => t.DueDate);
+        builder.Property(t => t.ResolvedDate);
 
-            builder.Property(t => t.ProjectId).IsRequired();
-            builder.Property(t => t.IssueStatusId).IsRequired();
-            builder.Property(t => t.IssueTypeId).IsRequired();
+        builder.Property(t => t.ProjectId).IsRequired();
+        builder.Property(t => t.IssueStatusId).IsRequired();
+        builder.Property(t => t.IssueTypeId).IsRequired();
 
-            builder.HasOne<Project>()
-                  .WithMany()
-                  .HasForeignKey(t => t.ProjectId)
-                  .IsRequired()
-                  .OnDelete(DeleteBehavior.Restrict);
+        builder.HasOne<Project>()
+              .WithMany()
+              .HasForeignKey(t => t.ProjectId)
+              .IsRequired()
+              .OnDelete(DeleteBehavior.Restrict);
 
-            //builder.HasOne(i => i.User)
-            //    .WithMany(u => u.Issues)
-            //    .HasForeignKey(i => i.ReporterId);
+        //builder.HasOne(i => i.User)
+        //    .WithMany(u => u.Issues)
+        //    .HasForeignKey(i => i.ReporterId);
 
-        }
     }
 }
---
+++ src/backend/Services/Task/TaskService.Infrastructure/Migrations/20260201125920_CreateIssueStatusAndType.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateIssueStatusAndType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "IssueStatus",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProjectId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    Position = table.Column<int>(type: "integer", nullable: false),
+                    Color = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "character varying(225)", maxLength: 225, nullable: false),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IssueStatus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IssueType",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProjectId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Color = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "character varying(225)", maxLength: 225, nullable: false),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IssueType", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IssueStatus");
+
+            migrationBuilder.DropTable(
+                name: "IssueType");
+        }
+    }
+}
