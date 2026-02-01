fff010cb80bb4676e7e2b273f73c5ed632f67e76
jlamma <jlampjlamma@gmail.com>
2026-02-01 14:07:48 +0300

    [feature] added command and handler for delete the issue status

---
+++ src/backend/Services/Task/TaskService.Application/Features/IssueStatuses/Handler/IssueStatusDeleteByIdHandler.cs
@@ -0,0 +1,18 @@
+﻿using TaskService.Application.Features.IssueStatuses.Command;
+using TaskService.Application.Mediator;
+using TaskService.Domain.Entities;
+using TaskService.Domain.Repositories;
+
+namespace TaskService.Application.Features.IssueStatuses.Handler;
+
+public class IssueStatusDeleteByIdHandler(IRepositoryWrapper wrapper) : IRequestHandler<IssueStatusDeleteByIdCommand, int>
+{
+    public async Task<int> Handle(IssueStatusDeleteByIdCommand request, CancellationToken cancellationToken = default)
+    {
+        IssueStatus status = await wrapper.IssueStatus.GetByIdAsync(request.id, cancellationToken) ??
+            throw new NullReferenceException($"Статус с id: {request.id} не найден");
+
+        await wrapper.IssueStatus.DeleteAsync(status, cancellationToken);
+        return await wrapper.SaveChangesAsync(cancellationToken);
+    }
+}
