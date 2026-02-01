be2399f30df81f566c16614692d7e20ea9c3b436
jlamma <jlampjlamma@gmail.com>
2026-02-01 17:34:03 +0300

    [feature] Added a check for related Issues to the IssueStatus and IssueType deletion handlers.

--- src/backend/Services/Task/TaskService.Application/Features/IssueTypes/Handler/IssueTypeDeleteByIdHandler.cs
+++ src/backend/Services/Task/TaskService.Application/Features/IssueTypes/Handler/IssueTypeDeleteByIdHandler.cs
@@ -1,4 +1,5 @@
-﻿using TaskService.Application.Features.IssueTypes.Command;
+﻿using System.Net.NetworkInformation;
+using TaskService.Application.Features.IssueTypes.Command;
 using TaskService.Application.Mediator;
 using TaskService.Contracts.IssueType;
 using TaskService.Domain.Entities;
@@ -12,6 +13,11 @@ namespace TaskService.Application.Features.IssueTypes.Handler
         {
             IssueType type = await wrapper.IssueType.GetByIdAsync(request.id, cancellationToken) ??
             throw new NullReferenceException($"Тип задачи с id: {request.id} не найден");
+
+            List<Issue> issue = await wrapper.Issues.GetByIssueTypeIdAsync(typeId: type.Id, cancellationToken);
+
+            if (issue != null && issue.Count > 0)
+                throw new NullReferenceException($"Нельзя удалить тип задачи, пока существуют связанные задачи");
 
             await wrapper.IssueType.DeleteAsync(type, cancellationToken);
             return await wrapper.SaveChangesAsync(cancellationToken);
