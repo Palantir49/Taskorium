9e6fb03e18a769cac5faebcc42d6de13452bdadb
jlamma <jlampjlamma@gmail.com>
2026-02-01 13:43:12 +0300

    [feature] added handlers get by id and project id for issue status

---
+++ src/backend/Services/Task/TaskService.Application/Features/IssueStatuses/Handler/IssueStatusGetByProjectIdHandler.cs
@@ -0,0 +1,18 @@
+﻿using TaskService.Application.Features.IssueStatuses.Command;
+using TaskService.Application.Mediator;
+using TaskService.Contracts.IssueStatus;
+using TaskService.Domain.Entities;
+using TaskService.Domain.Repositories;
+
+namespace TaskService.Application.Features.IssueStatuses.Handler
+{
+    internal class IssueStatusGetByProjectIdHandler(IRepositoryWrapper wrapper) : IRequestHandler<IssueStatusGetByProjectIdQuery, IEnumerable<IssueStatusResponse>>
+    {
+        public async Task<IEnumerable<IssueStatusResponse>> Handle(IssueStatusGetByProjectIdQuery request, CancellationToken cancellationToken = default)
+        {
+            List<IssueStatus> statuses = await wrapper.IssueStatus.GetByProjectIdAsync(request.id, cancellationToken);
+
+            return statuses.Select(x => x.ToResponse());
+        }
+    }
+}
