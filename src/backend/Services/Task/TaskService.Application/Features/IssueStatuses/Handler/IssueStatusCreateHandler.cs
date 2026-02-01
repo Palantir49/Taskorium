3d5aeb64d93a8248fc15e64429d684502c9f0746
jlamma <jlampjlamma@gmail.com>
2026-02-01 13:29:11 +0300

    [feature] added create handler for issue status

---
+++ src/backend/Services/Task/TaskService.Application/Features/IssueStatuses/Handler/IssueStatusCreateHandler.cs
@@ -0,0 +1,29 @@
+﻿using TaskService.Application.Commands.Projects;
+using TaskService.Application.Features.IssueStatuses.Command;
+using TaskService.Application.Mediator;
+using TaskService.Contracts.IssueStatus;
+using TaskService.Domain.Entities;
+using TaskService.Domain.Repositories;
+
+namespace TaskService.Application.Features.IssueStatuses.Handler;
+
+public class IssueStatusCreateHandler (IRepositoryWrapper wrapper) : IRequestHandler<IssueStatusCreateCommand, IssueStatusResponse>
+{
+    public async Task<IssueStatusResponse> Handle(IssueStatusCreateCommand request, CancellationToken cancellationToken = default)
+    {
+        Project project = await wrapper.Projects.GetByIdAsync(request.projectId, cancellationToken) ??
+            throw new NullReferenceException($"Проект с id: {request.projectId} не найден");
+
+        IssueStatus status = IssueStatus.Create(
+            name: request.name,
+            type: request.type,
+            position: request.position,
+            color: request.color,
+            projectId: request.projectId);
+
+        await wrapper.IssueStatus.AddAsync(status, cancellationToken);
+        await wrapper.SaveChangesAsync(cancellationToken);
+
+        return status.ToResponse();
+    }
+}
