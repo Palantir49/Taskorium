4779eda392fbcf23c83f8f513b9777a6207a03b9
jlamma <jlampjlamma@gmail.com>
2026-02-01 14:07:48 +0300

    [feature] added command and handler for delete the issue status

---
+++ src/backend/Services/Task/TaskService.Application/Features/IssueStatuses/Command/IssueStatusDeleteByIdCommand.cs
@@ -0,0 +1,5 @@
+﻿using TaskService.Application.Mediator;
+
+namespace TaskService.Application.Features.IssueStatuses.Command;
+
+public record class IssueStatusDeleteByIdCommand(Guid id) : ICommand<int>;
