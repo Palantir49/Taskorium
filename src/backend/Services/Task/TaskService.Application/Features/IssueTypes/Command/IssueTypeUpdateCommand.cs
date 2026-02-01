1890f4bb94d432f1dc8c5d0116bbd3fe91ad1f3b
jlamma <jlampjlamma@gmail.com>
2026-02-01 14:08:09 +0300

    [fix]

--- src/backend/Services/Task/TaskService.Application/Features/IssueTypes/Command/IssueTypeUpdateCommand.cs
+++ src/backend/Services/Task/TaskService.Application/Features/IssueTypes/Command/IssueTypeUpdateCommand.cs
@@ -5,4 +5,4 @@ namespace TaskService.Application.Features.IssueTypes.Command;
 public record class IssueTypeUpdateCommand(
     Guid id,
     string name,
-    string color) : ICommand<int>;
+    string? color) : ICommand<int>;
