1a66c544a8fe3d2a7fbf27979079f22eea544ec6
jlamma <jlampjlamma@gmail.com>
2026-02-01 17:10:07 +0300

    [feature] added a controller for issue type

--- src/backend/Services/Task/TaskService.Api/Controllers/IssuesController.cs
+++ src/backend/Services/Task/TaskService.Api/Controllers/IssuesController.cs
@@ -4,7 +4,6 @@ using TaskService.Application.Commands.Issues.Command;
 using TaskService.Application.Commands.Issues.Handler;
 using TaskService.Application.Features.Issues.Command;
 using TaskService.Application.Features.Issues.Mapping;
-using TaskService.Application.Features.IssueStatuses.Command;
 using TaskService.Application.Mediator;
 using TaskService.Contracts.Issue.Requests;
 using TaskService.Contracts.Issue.Responses;
