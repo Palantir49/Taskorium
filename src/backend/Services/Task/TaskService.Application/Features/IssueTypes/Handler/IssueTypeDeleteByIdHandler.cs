33f3035c2cf36f51c0b42a8dcb0ec6613fe6692b
jlamma <jlampjlamma@gmail.com>
2026-02-01 15:09:04 +0300

    [feature] added a command and a handler to delete issue types by id

---
+++ src/backend/Services/Task/TaskService.Application/Features/IssueTypes/Handler/IssueTypeDeleteByIdHandler.cs
@@ -0,0 +1,20 @@
+﻿using TaskService.Application.Features.IssueTypes.Command;
+using TaskService.Application.Mediator;
+using TaskService.Contracts.IssueType;
+using TaskService.Domain.Entities;
+using TaskService.Domain.Repositories;
+
+namespace TaskService.Application.Features.IssueTypes.Handler
+{
+    public class IssueTypeDeleteByIdHandler(IRepositoryWrapper wrapper) : IRequestHandler<IssueTypeDeleteByIdCommand, int>
+    {
+        public async Task<int> Handle(IssueTypeDeleteByIdCommand request, CancellationToken cancellationToken = default)
+        {
+            IssueType type = await wrapper.IssueType.GetByIdAsync(request.id, cancellationToken) ??
+            throw new NullReferenceException($"Тип задачи с id: {request.id} не найден");
+
+            await wrapper.IssueType.DeleteAsync(type, cancellationToken);
+            return await wrapper.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
