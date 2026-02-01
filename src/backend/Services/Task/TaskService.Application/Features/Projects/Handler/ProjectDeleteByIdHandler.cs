e68d65787a790b5ad70979226cdcb27990ab1906
jlamma <jlampjlamma@gmail.com>
2026-02-01 19:39:33 +0300

    [feature] added check for related issues to the project deletion handler

--- src/backend/Services/Task/TaskService.Application/Features/Projects/Handler/ProjectDeleteByIdHandler.cs
+++ src/backend/Services/Task/TaskService.Application/Features/Projects/Handler/ProjectDeleteByIdHandler.cs
@@ -11,6 +11,10 @@ public class ProjectDeleteByIdHandler(IRepositoryWrapper wrapper) : IRequestHand
     {
         Project project = await wrapper.Projects.GetByIdAsync(request.id, cancellationToken) ??
             throw new NullReferenceException($"Проект с id: {request.id} не найдена");
+        List<Issue> issues = await wrapper.Issues.GetByProjectIdAsync(projectId: project.Id, cancellationToken);
+        if (issues != null && issues.Count > 0)
+            throw new NullReferenceException($"Нельзя удалить статус, пока существуют связанные задачи");
+        //TODO: изменить исключение на подходящее
 
         await wrapper.Projects.DeleteAsync(project, cancellationToken);
         return await wrapper.SaveChangesAsync(cancellationToken);
