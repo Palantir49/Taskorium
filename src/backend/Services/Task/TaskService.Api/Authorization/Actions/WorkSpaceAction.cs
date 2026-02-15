07a0693863a79245e433b0cc7a145fbdd0d22396
palantir49 <pupkin.dus@yandex.ru>
2026-02-15 17:20:27 +0300

    [feature] add last migrations

---
+++ src/backend/Services/Task/TaskService.Api/Authorization/Actions/WorkSpaceAction.cs
@@ -0,0 +1,34 @@
+﻿namespace TaskService.Infrastructure.Authorization.Actions;
+
+/// <summary>
+/// </summary>
+public enum WorkSpaceAction
+{
+    /// <summary>
+    /// </summary>
+    View,
+
+    /// <summary>
+    /// </summary>
+    Create,
+
+    /// <summary>
+    /// </summary>
+    Update,
+
+    /// <summary>
+    /// </summary>
+    Delete,
+
+    /// <summary>
+    /// </summary>
+    AddUser,
+
+    /// <summary>
+    /// </summary>
+    DeleteUser,
+
+    /// <summary>
+    /// </summary>
+    UpdateUser
+}
