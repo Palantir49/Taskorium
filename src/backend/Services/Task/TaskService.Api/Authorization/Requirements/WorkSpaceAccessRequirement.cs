f5b99f39a184fbc77354ff697a6c231c20f8c5bd
palantir49 <pupkin.dus@yandex.ru>
2026-02-15 19:49:30 +0300

    [feature] Add requirements

---
+++ src/backend/Services/Task/TaskService.Api/Authorization/Requirements/WorkSpaceAccessRequirement.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using TaskService.Infrastructure.Authorization.Actions;
+
+namespace TaskService.Api.Authorization.Requirements;
+
+/// <summary>
+///     WorkSpaceAccessRequirement
+/// </summary>
+/// <param name="Action"></param>
+public record WorkSpaceAccessRequirement(WorkSpaceAction Action) : IAuthorizationRequirement;
