ea52db0c6a27d0cc5669c52efae36d2e79c9cb29
palantir49 <pupkin.dus@yandex.ru>
2026-02-15 19:53:12 +0300

    [feature] Add workspaces and project handlers

---
+++ src/backend/Services/Task/TaskService.Api/Authorization/Handlers/ProjectAccessHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using TaskService.Api.Authorization.Requirements;
+
+namespace TaskService.Api.Authorization.Handlers;
+
+/// <summary>
+///     Обработчик авторизации проектов
+/// </summary>
+public class ProjectAccessHandler : AuthorizationHandler<IssueAccessRequirement>
+{
+    /// <summary>
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="requirement"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+        IssueAccessRequirement requirement)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/backend/Services/Task/TaskService.Api/Authorization/Handlers/WorkSpaceAccessHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using TaskService.Api.Authorization.Requirements;
+
+namespace TaskService.Api.Authorization.Handlers;
+
+/// <summary>
+///     Обработчик авторизации рабочих областей
+/// </summary>
+public class WorkSpaceAccessHandler : AuthorizationHandler<WorkSpaceAccessRequirement>
+{
+    /// <summary>
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="requirement"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+        WorkSpaceAccessRequirement requirement)
+    {
+        throw new NotImplementedException();
+    }
+}
