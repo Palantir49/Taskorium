1e21b293aaf249dd251e3b8fd26e6fc06521e140
jlamma <jlampjlamma@gmail.com>
2026-02-22 16:57:00 +0300

    [feature] added getting a list value IssueStatusType

---
+++ src/backend/Services/Task/TaskService.Application/Features/Collections/Handler/GetAllIssueStatusTypeHandler.cs
@@ -0,0 +1,34 @@
+﻿using TaskService.Application.Features.Collections.Query;
+using TaskService.Application.Mediator;
+using TaskService.Contracts.Collections;
+using TaskService.Domain.Entities.Enums;
+
+namespace TaskService.Application.Features.Collections.Handler;
+
+
+public class GetAllIssueStatusTypeHandler : IRequestHandler<GetAllIssueStatusTypesQuery, IEnumerable<IssueStatusTypeResponse>>
+{
+    public async Task<IEnumerable<IssueStatusTypeResponse>> Handle(GetAllIssueStatusTypesQuery request, CancellationToken cancellationToken = default)
+    {
+        return Enum.GetValues<IssueStatusType>()
+            .Cast<IssueStatusType>()
+            .Select(x => new IssueStatusTypeResponse(
+                Number: (int)x,
+                Name: x.ToString(),
+                DisplayName: GetDisplayName(x)
+                ))
+            .ToList();
+    }
+
+    private string GetDisplayName(IssueStatusType type)
+    {
+        return type switch
+        {
+            IssueStatusType.Initial => "Новая",
+            IssueStatusType.Process => "В работе",
+            IssueStatusType.Success => "Выполнено",
+            IssueStatusType.Rejected => "Отменено",
+            _ => type.ToString()
+        };
+    }
+}
