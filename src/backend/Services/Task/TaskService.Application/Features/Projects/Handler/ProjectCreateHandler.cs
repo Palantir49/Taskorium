5842d3768ac2d4b14bb998750e028461dcdd6c9f
jlamma <jlampjlamma@gmail.com>
2026-03-01 16:59:02 +0300

    [feature] added the creation IssueStatus when creating a Project

--- src/backend/Services/Task/TaskService.Application/Features/Projects/Handler/ProjectCreateHandler.cs
+++ src/backend/Services/Task/TaskService.Application/Features/Projects/Handler/ProjectCreateHandler.cs
@@ -2,6 +2,7 @@
 using TaskService.Application.Mediator;
 using TaskService.Contracts.Project.Responses;
 using TaskService.Domain.Entities;
+using TaskService.Domain.Entities.Enums;
 using TaskService.Infrastructure.Persistence;
 
 
@@ -20,8 +21,44 @@ public class ProjectCreateHandler(TaskServiceDbContext context) : IRequestHandle
             abbreviation: request.Abbreviation,
             workspaceId: request.WorkspaceId
         );
-        //TODO: добавить создание статусов и типа
+
+        IssueStatus initStatus = IssueStatus.Create(
+            name: "Новая",
+            numberType: (int)IssueStatusType.Initial,
+            position: 0,
+            color: "#000000",
+            projectId: project.Id
+            );
+
+        IssueStatus processStatus = IssueStatus.Create(
+            name: "В работе",
+            numberType: (int)IssueStatusType.Process,
+            position: 1,
+            color: "#000000",
+            projectId: project.Id
+            );
+
+        IssueStatus successStatus = IssueStatus.Create(
+            name: "Выполнено",
+            numberType: (int)IssueStatusType.Success,
+            position: 2,
+            color: "#000000",
+            projectId: project.Id
+            );
+
+        IssueStatus rejectedStatus = IssueStatus.Create(
+            name: "Отменено",
+            numberType: (int)IssueStatusType.Rejected,
+            position: 3,
+            color: "#000000",
+            projectId: project.Id
+            );
+
         await context.Projects.AddAsync(project, cancellationToken);
+        await context.IssueStatus.AddAsync(initStatus, cancellationToken);
+        await context.IssueStatus.AddAsync(processStatus, cancellationToken);
+        await context.IssueStatus.AddAsync(successStatus, cancellationToken);
+        await context.IssueStatus.AddAsync(rejectedStatus, cancellationToken);
         await context.SaveChangesAsync(cancellationToken);
 
         return project.ToResponse();
