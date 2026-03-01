123e5279844e97f0e7f974de00e0b807727449d3
jlamma <jlampjlamma@gmail.com>
2026-03-01 14:21:06 +0300

    [feature] added IssueKey for issue creation handler

--- src/backend/Services/Task/TaskService.Application/Features/Issues/Handler/IssueCreateHandler.cs
+++ src/backend/Services/Task/TaskService.Application/Features/Issues/Handler/IssueCreateHandler.cs
@@ -4,6 +4,7 @@ using TaskService.Application.Features.Issues.Mapping;
 using TaskService.Application.Mediator;
 using TaskService.Contracts.Issue.Responses;
 using TaskService.Domain.Entities;
+using TaskService.Domain.Entities.Enums;
 using TaskService.Infrastructure.Persistence;
 
 namespace TaskService.Application.Commands.Issues.Handler;
@@ -12,22 +13,22 @@ public class IssueCreateHandler(TaskServiceDbContext context) : IRequestHandler<
 {
     public async Task<IssueResponse> Handle(IssueCreateCommand request, CancellationToken cancellationToken = default)
     {
-        var project = await context.Projects.FirstOrDefaultAsync(x => x.Id == request.ProjectId, cancellationToken) ??
+        Project project = await context.Projects.FirstOrDefaultAsync(x => x.Id == request.ProjectId, cancellationToken) ??
             throw new NullReferenceException($"Проект с id: {request.ProjectId} не найдена");
 
-        //TODO: создавать с статусом инициализации из проекта
-        IssueStatus? status = await context.IssueStatus.FindAsync(request.IssueStatusId, cancellationToken) ??
-            throw new NullReferenceException($"Статус задачи с id: {request.IssueStatusId} не найдена");
+        IssueStatus status = await context.IssueStatus.FindAsync(request.ProjectId, IssueStatusType.Initial, cancellationToken) ??
+            throw new NullReferenceException($"Не найден статус инициализации задачи для проекта {request.ProjectId}");
 
-        //TODO: проверить что можно создавать с этим статусом
+        int countIssue = await context.Issues.CountAsync(x=> x.ProjectId == project.Id, cancellationToken);
+
+        string issueKey = $"{project.Abbreviation}-{countIssue + 1}";
 
         var issue = Issue.Create(
             name: request.Name,
             description: request.Description,
-            key: "",
+            key: issueKey,
             projectId: request.ProjectId,
-            taskTagId: request.IssueTagId,
-            taskStatusId: request.IssueStatusId,
+            taskStatusId: status.Id,
             numberIssueType: request.numberIssueType,
             dueDate: request.DueDate
         );
