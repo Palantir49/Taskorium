29d8f1809b8df69272e935e7a4dd221df3eabfe5
tsunamiwave <task1337@bk.ru>
2026-03-29 18:12:10 +0300

    [code cleanup]

--- src/backend/Services/Task/TaskService.Infrastructure/Configurations/TagConfiguration.cs
+++ src/backend/Services/Task/TaskService.Infrastructure/Configurations/TagConfiguration.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using TaskService.Domain.Entities;
 using TaskService.Domain.ValueObjects;
