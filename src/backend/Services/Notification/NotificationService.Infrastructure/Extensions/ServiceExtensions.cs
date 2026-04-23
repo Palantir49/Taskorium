dd174b23e8a3f5e5160c02d178d67ec79b58314e
palantir49 <pupkin.dus@yandex.ru>
2026-04-23 23:30:01 +0300

    [feature] Add extensions to configire infrastructure layer

---
+++ src/backend/Services/Notification/NotificationService.Infrastructure/Extensions/ServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NotificationService.Infrastructure.Extensions.MessageBus;
+
+namespace NotificationService.Infrastructure.Extensions;
+
+public static class ServiceExtensions
+{
+    extension(IServiceCollection services)
+    {
+        public void ConfigureInfrastructureLayer(IConfiguration configuration)
+        {
+            services.AddMessageBus(configuration);
+        }
+    }
+}
