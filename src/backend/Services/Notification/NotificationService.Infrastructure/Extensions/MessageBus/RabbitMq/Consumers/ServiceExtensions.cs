622a2f6780ea428c0a803125869d93e07a56df80
palantir49 <pupkin.dus@yandex.ru>
2026-04-23 23:30:01 +0300

    [feature] Add extensions to configire infrastructure layer

---
+++ src/backend/Services/Notification/NotificationService.Infrastructure/Extensions/MessageBus/RabbitMq/Consumers/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NotificationService.Application.Integration.EventHandlers;
+using Taskorium.IntegrationEvents.Notifications;
+using Taskorium.MessageBus.RabbitMq;
+
+namespace NotificationService.Infrastructure.Extensions.MessageBus.RabbitMq.Consumers;
+
+internal static class ServiceExtensions
+{
+    extension(IServiceCollection serviceCollection)
+    {
+        internal void AddRabbitMqConsumers(IConfiguration configuration)
+        {
+            serviceCollection.AddRabbitMqConsumer<IssueCreatedIntegrationEvent, IssueCreatedEventHandler>(configuration,
+                "partner-manager-received");
+        }
+    }
+}
