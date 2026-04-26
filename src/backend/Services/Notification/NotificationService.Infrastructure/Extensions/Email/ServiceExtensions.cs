51092ae5c080c5967ad8ae93732d4132ae7721c0
palantir49 <pupkin.dus@yandex.ru>
2026-04-26 18:51:38 +0300

    [feature] Configure Email service

---
+++ src/backend/Services/Notification/NotificationService.Infrastructure/Extensions/Email/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NotificationService.Application.Interfaces.Email;
+using NotificationService.Infrastructure.Email.Options;
+using NotificationService.Infrastructure.Email.Services;
+
+namespace NotificationService.Infrastructure.Extensions.Email;
+
+internal static class ServiceExtensions
+{
+    extension(IServiceCollection serviceCollection)
+    {
+        internal void ConfigureEmailSenderService(IConfiguration configuration)
+        {
+            serviceCollection
+                .AddOptions<SmtpOptions>()
+                .Bind(configuration.GetSection(SmtpOptions.SectionName))
+                .ValidateDataAnnotations()
+                .ValidateOnStart();
+
+
+            serviceCollection.AddScoped<IEmailSenderService, EmailSenderService>();
+        }
+    }
+}
--- src/backend/Services/Notification/NotificationService.Infrastructure/Extensions/ServiceExtensions.cs
+++ src/backend/Services/Notification/NotificationService.Infrastructure/Extensions/ServiceExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NotificationService.Infrastructure.Extensions.Email;
 using NotificationService.Infrastructure.Extensions.MessageBus;
 using NotificationService.Infrastructure.Extensions.Persistence;
 
@@ -13,6 +14,7 @@ public static class ServiceExtensions
         {
             services.AddMessageBus(configuration);
             services.AddPersistence(configuration);
+            services.ConfigureEmailSenderService(configuration);
         }
     }
 }
