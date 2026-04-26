ad416dba0cdafa8229a392c8391b730a2465f8e9
palantir49 <pupkin.dus@yandex.ru>
2026-04-26 17:22:26 +0300

    [feature] Configure persistense (mongo)

---
+++ src/backend/Services/Notification/NotificationService.Infrastructure/Extensions/Persistence/Mongo/ServiceExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using NotificationService.Infrastructure.Persistence.Mongo.Contexts;
+using NotificationService.Infrastructure.Persistence.Mongo.Options;
+
+namespace NotificationService.Infrastructure.Extensions.Persistence.Mongo;
+
+internal static class ServiceExtensions
+{
+    extension(IServiceCollection serviceCollection)
+    {
+        internal void AddMongo(IConfiguration configuration)
+        {
+            var mongoDbOptions = configuration
+                .GetSection(MongoDbOptions.SectionName)
+                .Get<MongoDbOptions>();
+
+            ArgumentNullException.ThrowIfNull(mongoDbOptions);
+
+            var credential = MongoCredential.CreateCredential(mongoDbOptions.AuthSource, mongoDbOptions.Username,
+                mongoDbOptions.Password);
+
+            serviceCollection.AddSingleton<IMongoClient>(_ =>
+            {
+                var clientSettings = new MongoClientSettings
+                {
+                    Credential = credential,
+                    Server = new MongoServerAddress(mongoDbOptions.Host, mongoDbOptions.Port)
+                };
+
+                return new MongoClient(clientSettings);
+            });
+
+            serviceCollection.AddScoped<NotificationDbContext>();
+        }
+    }
+}
---
+++ src/backend/Services/Notification/NotificationService.Infrastructure/Persistence/Mongo/Contexts/NotificationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Driver;
+using NotificationService.Domain.Aggregates.Notification;
+
+namespace NotificationService.Infrastructure.Persistence.Mongo.Contexts;
+
+public class NotificationDbContext(IMongoClient mongoClient)
+{
+    private readonly IMongoDatabase _database = mongoClient.GetDatabase("NotificationDb");
+
+    public IMongoCollection<Notification> Notifications =>
+        _database.GetCollection<Notification>("Notifications");
+}
