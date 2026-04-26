75f88e0ddf63b9c6a6b6ce4c0061ec20acb6d9e8
palantir49 <pupkin.dus@yandex.ru>
2026-04-26 16:46:51 +0300

    [feature] Setup notification service

---
+++ src/backend/Services/Notification/NotificationService.Api/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.OpenApi;
+using NotificationService.Infrastructure.Extensions;
+using Scalar.AspNetCore;
+using Taskorium.ServiceDefaults;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.Setup(builder.Environment.EnvironmentName);
+builder.Host.ValidateServices();
+builder.Services.AddServiceDefaults(builder.Configuration);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document, context, cancellationToken) =>
+    {
+        document.Info.Version = "1.0.0-alpha.1";
+        document.Info.Title = "Сервис уведомлений";
+        document.Info.Description = "Сервис уведомлений";
+        document.Info.Contact = new OpenApiContact
+        {
+            Name = "https://github.com/Palantir49",
+            Email = "Vadim Ryzhenkov",
+            Url = new Uri("https://github.com/Palantir49")
+        };
+
+        document.Info.License = new OpenApiLicense
+        {
+            Name = "MIT License",
+            Url = new Uri("https://opensource.org/licenses/MIT")
+        };
+        return Task.CompletedTask;
+    });
+});
+builder.Services.ConfigureInfrastructureLayer(builder.Configuration);
+var app = builder.Build();
+app.UseServiceDefaults(builder.Configuration);
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.Run();
