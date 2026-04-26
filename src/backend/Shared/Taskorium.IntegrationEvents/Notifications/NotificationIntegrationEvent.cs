85178ca1c0ce7c107036acbad3a2dbea677e4f0e
palantir49 <pupkin.dus@yandex.ru>
2026-04-26 18:54:37 +0300

    [fix] rename

--- src/backend/Shared/Taskorium.IntegrationEvents/Notifications/NotificationIntegrationEvent.cs
+++
@@ -1,7 +0,0 @@
-﻿using Taskorium.IntegrationEvents.Dto;
-using Taskorium.MessageBus.Abstractions;
-
-namespace Taskorium.IntegrationEvents.Notifications;
-
-public record NotificationIntegrationEvent(NotificationEventContent Content, List<NotificationRecipient> Recipients)
-    : IntegrationEvent;
