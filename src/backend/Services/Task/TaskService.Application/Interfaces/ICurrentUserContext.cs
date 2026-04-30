19f677bce105505e2e069ee56114af1ce3b16418
jlamma <jlampjlamma@gmail.com>
2026-04-30 14:51:58 +0300

    [feature] created interface of current user context

---
+++ src/backend/Services/Task/TaskService.Application/Interfaces/ICurrentUserContext.cs
@@ -0,0 +1,10 @@
+﻿using TaskService.Application.Features.Users.Get;
+
+namespace TaskService.Application.Interfaces;
+
+public interface ICurrentUserContext
+{
+    GetUserByKeycloakIdResult? User { get; }
+    bool IsInitialized { get; }
+    void Initialize(GetUserByKeycloakIdResult user);
+}
