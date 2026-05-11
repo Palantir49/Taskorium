097084249b2a0f62ff993f7124a064d640ea46f1
palantir49 <pupkin.dus@yandex.ru>
2026-05-11 21:29:07 +0300

    [feature] Add transaction session, add indexes

--- src/backend/Services/Notification/NotificationService.Infrastructure/Persistence/Mongo/Contexts/NotificationDbContext.cs
+++ src/backend/Services/Notification/NotificationService.Infrastructure/Persistence/Mongo/Contexts/NotificationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Driver;
 using NotificationService.Domain.Aggregates.Notification;
+using NotificationService.Domain.Aggregates.Outbox;
 
 namespace NotificationService.Infrastructure.Persistence.Mongo.Contexts;
 
@@ -9,4 +10,25 @@ public class NotificationDbContext(IMongoClient mongoClient)
 
     public IMongoCollection<Notification> Notifications =>
         _database.GetCollection<Notification>("Notifications");
+
+    public IMongoCollection<OutBoxMessage> OutBoxMessages =>
+        _database.GetCollection<OutBoxMessage>("OutBoxMessages");
+
+    internal IClientSessionHandle? CurrentSession { get; set; }
+
+    public async Task<IClientSessionHandle> StartSessionAsync(CancellationToken cancellationToken = default)
+    {
+        return await mongoClient.StartSessionAsync(cancellationToken: cancellationToken);
+    }
+
+    public async Task EnsureNotificationIndexesAsync(CancellationToken ct)
+    {
+        var idempotencyIndex = new CreateIndexModel<Notification>(
+            Builders<Notification>.IndexKeys
+                .Ascending("EventIdempotencyKey"),
+            new CreateIndexOptions { Unique = true, Name = "idx_idempotency_key_unique" });
+
+        await Notifications.Indexes
+            .CreateOneAsync(idempotencyIndex, cancellationToken: ct);
+    }
 }
