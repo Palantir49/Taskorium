b0d27e79af77d51e84f8e480456cd3c72e19a3be
palantir49 <pupkin.dus@yandex.ru>
2026-05-11 21:47:04 +0300

    [feature] Add and realize command, handler,result for mediator

---
+++ src/backend/Services/Notification/NotificationService.Application/Features/Notification/CreateNotification/Command/CreateNotificationCommand.cs
@@ -0,0 +1,12 @@
+﻿using NotificationService.Application.Abstractions;
+using NotificationService.Application.Features.Notification.CreateNotification.Result;
+using Taskorium.IntegrationEvents.Dto;
+
+namespace NotificationService.Application.Features.Notification.CreateNotification.Command;
+
+public record CreateNotificationCommand(
+    Guid IdempotencyKey,
+    DateTimeOffset CreatedAtUtc,
+    NotificationEventContent Content,
+    List<NotificationRecipient> Recipients
+) : ICommand<CreateNotificationResult>;
