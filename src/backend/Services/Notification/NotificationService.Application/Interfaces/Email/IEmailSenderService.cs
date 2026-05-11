491c2d58fb1c800be6d1c532c6411897528c567a
palantir49 <pupkin.dus@yandex.ru>
2026-05-11 21:22:19 +0300

    [fix] Delete unused contract

--- src/backend/Services/Notification/NotificationService.Application/Interfaces/Email/IEmailSenderService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace NotificationService.Application.Interfaces.Email;
-
-public interface IEmailSenderService
-{
-    Task SendEmailAsync(string email, string subject, string message, CancellationToken cancellationToken = default);
-}
