b0cda8ffdcae9cfbeb06198836895f644971a799
palantir49 <pupkin.dus@yandex.ru>
2026-05-11 21:51:26 +0300

    [feature] Add notification mapper

---
+++ src/backend/Services/Notification/NotificationService.Application/Mapping/NotificationMapper.cs
@@ -0,0 +1,13 @@
+﻿using NotificationService.Application.Features.Notification.CreateNotification.Command;
+using Riok.Mapperly.Abstractions;
+using Taskorium.IntegrationEvents.Notifications;
+
+namespace NotificationService.Application.Mapping;
+
+[Mapper]
+public partial class NotificationMapper
+{
+    [MapProperty(nameof(NotificationCreatedIntegrationEvent.Id), nameof(CreateNotificationCommand.IdempotencyKey))]
+    public partial CreateNotificationCommand MapToCreateNotificationCommand(
+        NotificationCreatedIntegrationEvent @event);
+}
