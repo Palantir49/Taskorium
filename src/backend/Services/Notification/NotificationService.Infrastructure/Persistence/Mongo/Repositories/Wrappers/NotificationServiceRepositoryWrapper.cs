7919a4e5e518ed559d014fbe98b08d32e164d131
palantir49 <pupkin.dus@yandex.ru>
2026-05-11 21:41:27 +0300

    [feature] Realize repository wrapper methods and properties: outbox, for transaction managing

--- src/backend/Services/Notification/NotificationService.Infrastructure/Persistence/Mongo/Repositories/Wrappers/NotificationServiceRepositoryWrapper.cs
+++ src/backend/Services/Notification/NotificationService.Infrastructure/Persistence/Mongo/Repositories/Wrappers/NotificationServiceRepositoryWrapper.cs
@@ -1,7 +1,9 @@
 ﻿using NotificationService.Domain.Repositories.Interfaces.Notifications;
+using NotificationService.Domain.Repositories.Interfaces.OutBox;
 using NotificationService.Domain.Repositories.Wrappers;
 using NotificationService.Infrastructure.Persistence.Mongo.Contexts;
 using NotificationService.Infrastructure.Persistence.Mongo.Repositories.Notification;
+using NotificationService.Infrastructure.Persistence.Mongo.Repositories.OutBox;
 
 namespace NotificationService.Infrastructure.Persistence.Mongo.Repositories.Wrappers;
 
@@ -15,5 +17,52 @@ public class NotificationServiceRepositoryWrapper(NotificationDbContext notifica
             field ??= new NotificationRepository(notificationDbContext);
             return field;
         }
+    }
+
+    public IOutBoxRepository OutBoxRepository
+    {
+        get
+        {
+            field ??= new OutboxRepository(notificationDbContext);
+            return field;
+        }
+    }
+
+    public async Task BeginTransactionAsync(CancellationToken ct = default)
+    {
+        notificationDbContext.CurrentSession = await notificationDbContext.StartSessionAsync(ct);
+        notificationDbContext.CurrentSession.StartTransaction();
+    }
+
+    public async Task CommitAsync(CancellationToken ct = default)
+    {
+        EnsureSession();
+        await notificationDbContext.CurrentSession!.CommitTransactionAsync(ct);
+        DisposeSession();
+    }
+
+    public async Task RollbackAsync(CancellationToken ct = default)
+    {
+        if (notificationDbContext.CurrentSession is not null)
+        {
+            await notificationDbContext.CurrentSession.CommitTransactionAsync(ct);
+            DisposeSession();
+        }
+    }
+
+    private void EnsureSession()
+    {
+        if (notificationDbContext.CurrentSession is null)
+        {
+            throw new InvalidOperationException(
+                "No active transaction. Call BeginTransactionAsync first.");
+        }
+    }
+
+
+    private void DisposeSession()
+    {
+        notificationDbContext.CurrentSession?.Dispose();
+        notificationDbContext.CurrentSession = null;
     }
 }
