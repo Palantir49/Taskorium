64afd4666fa56683a76439bf255d44dd589fb49c
palantir49 <pupkin.dus@yandex.ru>
2026-05-11 21:50:36 +0300

    [featire] Add notfication sender dispatcher

---
+++ src/backend/Services/Notification/NotificationService.Application/Interfaces/NotificationSenders/INotificationSender.cs
@@ -0,0 +1,8 @@
+﻿using NotificationService.Domain.Aggregates.Notification;
+
+namespace NotificationService.Application.Interfaces.NotificationSenders;
+
+public interface INotificationSender
+{
+    Task SendAsync(Notification notification, CancellationToken cancellationToken = default);
+}
