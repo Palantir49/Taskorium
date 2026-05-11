f87ff065e39f865ac7bc97586135ec8de7ca0e4d
palantir49 <pupkin.dus@yandex.ru>
2026-05-11 21:54:51 +0300

    [feature] Configure notification senders

---
+++ src/backend/Services/Notification/NotificationService.Infrastructure/Extensions/NotificationSenders/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NotificationService.Application.Interfaces.NotificationSenders;
+using NotificationService.Infrastructure.NotificationSenders.Options;
+using NotificationService.Infrastructure.NotificationSenders.Services;
+
+namespace NotificationService.Infrastructure.Extensions.NotificationSenders;
+
+internal static class ServiceExtensions
+{
+    extension(IServiceCollection serviceCollection)
+    {
+        internal void ConfigureEmailSenderService(IConfiguration configuration)
+        {
+            serviceCollection
+                .AddOptions<SmtpOptions>()
+                .Bind(configuration.GetSection(SmtpOptions.SectionName))
+                .ValidateDataAnnotations()
+                .ValidateOnStart();
+
+            serviceCollection.AddScoped<INotificationSender, NotificationSender>();
+            serviceCollection.AddScoped<IChannelSender, EmailSenderService>();
+        }
+    }
+}
