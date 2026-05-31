3238299ad900c71a6a87e8bd72ec882bc23d7470
tsunamiwave <task1337@bk.ru>
2026-05-31 19:16:39 +0300

    [feature] added workspace validators

---
+++ src/backend/Services/Task/TaskService.Application/Validators/Workspace/CreateWorkspaceCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+using TaskService.Application.Features.Workspaces.Write.CreateWorkspace;
+
+namespace TaskService.Application.Validators.Workspace
+{
+    public class CreateWorkspaceCommandValidator : AbstractValidator<CreateWorkspaceCommand>
+    {
+        public CreateWorkspaceCommandValidator()
+        {
+            RuleFor(x => x.OwnerId)
+                .NotEmpty()
+                .WithMessage("Ошибка создания рабочей области. Идентификатор создателя не может быть пустым");
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .Matches(@"^[a-zA-Zа-яА-ЯёЁ0-9 _\-.]+$")
+                .WithMessage("Имя может содержать только буквы, цифры, пробелы, дефисы, подчёркивания и точки.")
+                .MaximumLength(50)
+                .WithMessage("Имя рабочей области не может привышать 50 символов");
+
+
+        }
+    }
+}
