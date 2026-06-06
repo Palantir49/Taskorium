dc382b64f8db0c0dbbb73fbda12b484c217da3d5
jlamma <jlampjlamma@gmail.com>
2026-06-06 20:17:50 +0300

    [feature] implemented deletion for WorkspaceMember

---
+++ src/backend/Services/Task/TaskService.Application/Features/WorkspaceMembers/Write/DeleteWorkspaceMember/DeleteWorkspaceMemberCommand.cs
@@ -0,0 +1,6 @@
+﻿using TaskService.Application.Mediator;
+
+namespace TaskService.Application.Features.WorkspaceMembers.Write.DeleteWorkspaceMember
+{
+    public record DeleteWorkspaceMemberCommand(Guid WorkspaceId, Guid UserId) : ICommand<int>;
+}
