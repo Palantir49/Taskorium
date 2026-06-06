d35d016641d89331edf31e7e17b4919a973b6097
jlamma <jlampjlamma@gmail.com>
2026-06-06 20:23:05 +0300

    [feature] implemented deletion for ProjectMember

---
+++ src/backend/Services/Task/TaskService.Application/Features/ProjectMembers/Write/DeleteProjectMember/DeleteProjectMemberCommand.cs
@@ -0,0 +1,6 @@
+﻿using TaskService.Application.Mediator;
+
+namespace TaskService.Application.Features.ProjectMembers.Write.DeleteProjectMember
+{
+    public record DeleteProjectMemberCommand(Guid ProjectId, Guid UserId) : ICommand<int>;
+}
