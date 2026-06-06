20a791bf84a937069dd914ff875ca0f16bd99988
jlamma <jlampjlamma@gmail.com>
2026-06-06 23:22:18 +0300

    [feature] getting a list of legal successors has been implemented

---
+++ src/backend/Services/Task/TaskService.Application/Features/IssueAssignee/GetIssueAssigneesByIssueId/GetIssueAssigneesByIssueIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using TaskService.Application.Mediator;
+using TaskService.Contracts.IssueAssignees;
+
+namespace TaskService.Application.Features.IssueAssignee.GetIssueAssigneesByIssueId
+{
+    public record GetIssueAssigneesByIssueIdQuery(Guid IssueId) : IQuery<IEnumerable<IssueAssigneesResponce>>;
+}
