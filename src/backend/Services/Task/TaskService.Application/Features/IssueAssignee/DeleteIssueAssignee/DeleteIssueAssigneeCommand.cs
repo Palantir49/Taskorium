534f63b7c32025b290159dbea4af2f8fcd09acff
jlamma <jlampjlamma@gmail.com>
2026-06-06 23:13:30 +0300

    [feature] implemented IssueAssignee remove

---
+++ src/backend/Services/Task/TaskService.Application/Features/IssueAssignee/DeleteIssueAssignee/DeleteIssueAssigneeCommand.cs
@@ -0,0 +1,6 @@
+﻿using TaskService.Application.Mediator;
+
+namespace TaskService.Application.Features.IssueAssignee.DeleteIssueAssignee
+{
+    public record DeleteIssueAssigneeCommand(Guid IssueId, Guid UserId) : ICommand<int>;
+}
