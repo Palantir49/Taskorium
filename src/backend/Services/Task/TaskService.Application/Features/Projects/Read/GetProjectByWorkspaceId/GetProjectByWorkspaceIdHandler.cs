ae129e045a221e7509668903cec7f284b40cda47
palantir49 <pupkin.dus@yandex.ru>
2026-06-10 21:43:56 +0300

    feature: Add role to response

--- src/backend/Services/Task/TaskService.Application/Features/Projects/Read/GetProjectByWorkspaceId/GetProjectByWorkspaceIdHandler.cs
+++ src/backend/Services/Task/TaskService.Application/Features/Projects/Read/GetProjectByWorkspaceId/GetProjectByWorkspaceIdHandler.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Hybrid;
-using TaskService.Application.Commands.Projects;
+using TaskService.Application.Interfaces;
 using TaskService.Application.Mediator;
 using TaskService.Contracts.Project.Responses;
 using TaskService.Infrastructure.Persistence;
 
 namespace TaskService.Application.Features.Projects.Read.GetProjectByWorkspaceId;
 
-public class GetProjectByWorkspaceIdHandler(TaskServiceDbContext context, HybridCache cache)
+public class GetProjectByWorkspaceIdHandler(
+    TaskServiceDbContext context,
+    HybridCache cache,
+    ICurrentUserContext currentUserContext)
     : IRequestHandler<GetProjectByWorkspaceIdQuery, IEnumerable<ProjectResponse>>
 {
     public async Task<IEnumerable<ProjectResponse>> Handle(GetProjectByWorkspaceIdQuery request,
@@ -21,10 +24,11 @@ public class GetProjectByWorkspaceIdHandler(TaskServiceDbContext context, Hybrid
             {
                 var projects = await context.Projects
                     .AsNoTracking()
+                    .Include(x => x.ProjectMembers)
                     .Where(x => x.WorkspaceId == request.id)
                     .ToListAsync(token);
 
-                return projects.Select(x => x.ToResponse()).ToList();
+                return projects.Select(x => x.ToResponse(currentUserContext.User.Id)).ToList();
             },
             cancellationToken: cancellationToken);
     }
