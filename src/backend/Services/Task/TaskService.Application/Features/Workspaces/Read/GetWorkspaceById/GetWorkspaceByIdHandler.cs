d6c15f57f9a46500ff2d40fda06381d715ee9086
jlamma <jlampjlamma@gmail.com>
2026-06-12 17:05:57 +0300

    [feature] changed the GetWorkspaceById handler for testing

--- src/backend/Services/Task/TaskService.Application/Features/Workspaces/Read/GetWorkspaceById/GetWorkspaceByIdHandler.cs
+++ src/backend/Services/Task/TaskService.Application/Features/Workspaces/Read/GetWorkspaceById/GetWorkspaceByIdHandler.cs
@@ -4,24 +4,28 @@ using TaskService.Infrastructure.Persistence;
 
 namespace TaskService.Application.Features.Workspaces.Read.GetWorkspaceById;
 
-public class GetWorkspaceHandler(TaskServiceDbContext context, HybridCache cache)
+public class GetWorkspaceByIdHandler(TaskServiceDbContext context, HybridCache cache)
     : IRequestHandler<GetWorkspaceByIdQuery, GetWorkspacebyIdResult>
 {
     public async Task<GetWorkspacebyIdResult> Handle(GetWorkspaceByIdQuery query, CancellationToken cancellationToken)
     {
         var cacheKey = $"workspace_{query.Id}";
 
-        return await cache.GetOrCreateAsync(cacheKey, async _ =>
-        {
-            var workspace = await context.Workspaces.FindAsync([query.Id], cancellationToken);
-            if (workspace == null)
-            {
-                throw new KeyNotFoundException($"Рабочая область с id: {query.Id} не найдена");
-            }
+        return await cache.GetOrCreateAsync(cacheKey,
+            async ct => await GetWorkspaceFromDbAsync(query.Id, ct),
+            cancellationToken: cancellationToken);
+    }
 
-            return new GetWorkspacebyIdResult(
-                workspace.Id,
-                workspace.Name.ToString());
-        }, cancellationToken: cancellationToken);
+    public async Task<GetWorkspacebyIdResult> GetWorkspaceFromDbAsync(Guid workspaceId, CancellationToken cancellationToken)
+    {
+        var workspace = await context.Workspaces.FindAsync([workspaceId], cancellationToken);
+        if (workspace == null)
+        {
+            throw new KeyNotFoundException($"Рабочая область с id: {workspaceId} не найдена");
+        }
+
+        return new GetWorkspacebyIdResult(
+            workspace.Id,
+            workspace.Name.ToString());
     }
 }
