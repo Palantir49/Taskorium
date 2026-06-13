f13ead9ca66ad613b6437b77350292787b1c6757
jlamma <jlampjlamma@gmail.com>
2026-06-13 19:24:24 +0300

    [codecleanup]

--- src/backend/Services/Task/TaskService.Tests/WorkspaceTest/UpdateWorkspaceNameHandlerTest.cs
+++ src/backend/Services/Task/TaskService.Tests/WorkspaceTest/UpdateWorkspaceNameHandlerTest.cs
@@ -2,14 +2,10 @@
 using FluentAssertions;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
-using TaskService.Application.Features.Workspaces.Write.CreateWorkspace;
-using TaskService.Application.Features.Workspaces.Write.DeleteWorkspaceById;
 using TaskService.Application.Features.Workspaces.Write.UpdateWorkspaceName;
 using TaskService.Application.Validators.Workspace;
-using TaskService.Application.Validators.WorkspaceMember;
 using TaskService.Domain.Entities;
 using TaskService.Infrastructure.Persistence;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace TaskService.Tests.WorkspaceTest;
 
