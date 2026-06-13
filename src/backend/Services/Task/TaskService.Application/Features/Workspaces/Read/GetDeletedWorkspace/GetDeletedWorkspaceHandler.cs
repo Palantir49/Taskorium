a05857d623b333ab2ee61ed730b056f06044fce2
jlamma <jlampjlamma@gmail.com>
2026-06-13 12:35:43 +0300

    [codecleanup]

--- src/backend/Services/Task/TaskService.Application/Features/Workspaces/Read/GetDeletedWorkspace/GetDeletedWorkspaceHandler.cs
+++ src/backend/Services/Task/TaskService.Application/Features/Workspaces/Read/GetDeletedWorkspace/GetDeletedWorkspaceHandler.cs
@@ -1,29 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using TaskService.Application.Mediator;
 using TaskService.Contracts.Workspace.Response;
 using TaskService.Infrastructure.Persistence;
 
-namespace TaskService.Application.Features.Workspaces.Read.GetDeletedWorkspace
-{
-    public class GetDeletedWorkspaceHandler(TaskServiceDbContext context) : IRequestHandler<GetDeletedWorkspacePageQuery, GetDeletedWorkspacePageResult>
-    {
-        public async Task<GetDeletedWorkspacePageResult> Handle(GetDeletedWorkspacePageQuery query, CancellationToken cancellationToken)
-        {
-            var deletedWorkspaces = context.Workspaces.AsNoTracking()
-                                                      .IgnoreQueryFilters(["SoftDelete"])
-                                                      .Where(x => x.IsDeleted)
-                                                      .OrderByDescending(p => p.DeletedAt)
-                                                      .Skip(query.Skip)
-                                                      .Take(query.Take);
+namespace TaskService.Application.Features.Workspaces.Read.GetDeletedWorkspace;
 
-            var response = await deletedWorkspaces.Select(x => new DeletedWorkspaceResponse(Id: x.Id,
-                                                                                            Name: x.Name.Value,
-                                                                                            DeletedAt: x.DeletedAt!.Value))
-                                                  .ToListAsync();
-            return new GetDeletedWorkspacePageResult(workspaces: response);
-        }
+public class GetDeletedWorkspaceHandler(TaskServiceDbContext context) : IRequestHandler<GetDeletedWorkspacePageQuery, GetDeletedWorkspacePageResult>
+{
+    public async Task<GetDeletedWorkspacePageResult> Handle(GetDeletedWorkspacePageQuery query, CancellationToken cancellationToken)
+    {
+        var deletedWorkspaces = context.Workspaces.AsNoTracking()
+                                                  .IgnoreQueryFilters(["SoftDelete"])
+                                                  .Where(x => x.IsDeleted)
+                                                  .OrderByDescending(p => p.DeletedAt)
+                                                  .Skip(query.Skip)
+                                                  .Take(query.Take);
+
+        var response = await deletedWorkspaces.Select(x => new DeletedWorkspaceResponse(Id: x.Id,
+                                                                                        Name: x.Name.Value,
+                                                                                        DeletedAt: x.DeletedAt!.Value))
+                                              .ToListAsync();
+        return new GetDeletedWorkspacePageResult(workspaces: response);
     }
 }
