504b6a665e938c9ad35ee5c83dd0e3292c7f094e
jlamma <jlampjlamma@gmail.com>
2026-06-13 10:47:15 +0300

    [refactor] separated the logic of getting WorkspaceMembers from the cache

--- src/backend/Services/Task/TaskService.Application/Features/Workspaces/Read/GetWorkspaceMembers/GetWorkspaceMembersHandler.cs
+++ src/backend/Services/Task/TaskService.Application/Features/Workspaces/Read/GetWorkspaceMembers/GetWorkspaceMembersHandler.cs
@@ -15,26 +15,29 @@ namespace TaskService.Application.Features.Workspaces.Read.GetWorkspaceMembers
         {
             var cacheKey = $"workspaceMembers_{request.Id}";
 
-            return await cache.GetOrCreateAsync(cacheKey, async _ =>
-            {
-                var existProject = await context.Workspaces.Include(x => x.WorkspaceMembers)
+            return await cache.GetOrCreateAsync(cacheKey,
+                async ct => await GetWorkspaceMembersFromDbAsync(request.Id, ct),
+                cancellationToken: cancellationToken);
+        }
+
+        public async Task<WorkspaceMembersResponse> GetWorkspaceMembersFromDbAsync(Guid id, CancellationToken cancellationToken)
+        {
+            var existProject = await context.Workspaces.Include(x => x.WorkspaceMembers)
                                                          .ThenInclude(x => x.User)
                                                          .AsSplitQuery()
                                                          .AsNoTracking()
-                                                         .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
-                if (existProject is null)
-                    throw new ArgumentNullException($"Рабочая область с id: {request.Id} не найден");
+                                                         .FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+            if (existProject is null)
+                throw new ArgumentNullException($"Рабочая область с id: {id} не найден");
 
-                var members = new List<WorkspaceUserDto>();
+            var members = new List<WorkspaceUserDto>();
 
-                foreach (var x in existProject.WorkspaceMembers)
-                {
-                    members.Add(new WorkspaceUserDto(Id: x.User.Id, KeycloakId: x.User.KeycloakId, Role: x.Role.ToDto(), JoinedAt: x.JoinedAt, Email: x.User.Email.Value, UserName: x.User.Username.Value));
-                }
+            foreach (var x in existProject.WorkspaceMembers)
+            {
+                members.Add(new WorkspaceUserDto(Id: x.User.Id, KeycloakId: x.User.KeycloakId, Role: x.Role.ToDto(), JoinedAt: x.JoinedAt, Email: x.User.Email.Value, UserName: x.User.Username.Value));
+            }
 
-                return new WorkspaceMembersResponse(WorkspaceId: existProject.Id, WorkspaceName: existProject.Name.Value, Members: members);
-
-            }, cancellationToken: cancellationToken);
+            return new WorkspaceMembersResponse(WorkspaceId: existProject.Id, WorkspaceName: existProject.Name.Value, Members: members);
         }
     }
 }
