0df99e491a71e623d5d983ef5ce194aecda81db8
jlamma <jlampjlamma@gmail.com>
2026-06-13 22:20:06 +0300

    [test] GetProjectMembers from db

--- src/backend/Services/Task/TaskService.Tests/ProjectTest/GetProjectMembersFromDbTest.cs
+++ src/backend/Services/Task/TaskService.Tests/ProjectTest/GetProjectMembersFromDbTest.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using TaskService.Application.Features.Projects.Read.GetProjectMembers;
+using TaskService.Application.Mapping;
 using TaskService.Domain.Entities;
 using TaskService.Infrastructure.Persistence;
 
@@ -31,6 +32,12 @@ public class GetProjectMembersFromDbTest : IDisposable
                 workspaceId: _fixture.Create<Guid>()
                 ));
 
+        _fixture.Register(() => User.Create(
+            keycloakId: _fixture.Create<Guid>(),
+            userName: _fixture.Create<string>(),
+            email: _fixture.Create<string>(),
+            fullName: _fixture.Create<string>()));
+
         var options = new DbContextOptionsBuilder<TaskServiceDbContext>()
             .UseInMemoryDatabase(databaseName: $"TestDb_{Guid.NewGuid()}")
             .Options;
@@ -58,6 +65,7 @@ public class GetProjectMembersFromDbTest : IDisposable
         _disposed = true;
     }
 
+    [Fact]
     public async Task GetProjectMembersFromDb_WhenProjectNotExist_TrowKeyNotFoundException()
     {
         // ARRANGE
@@ -78,6 +86,7 @@ public class GetProjectMembersFromDbTest : IDisposable
             .WithMessage($"*{guid} не найден*");
     }
 
+    [Fact]
     public async Task GetProjectMembersFromDb_WhenProjectExistAndProjectMembersNotExist_ReturnEmptyResult()
     {
         // ARRANGE
@@ -96,6 +105,78 @@ public class GetProjectMembersFromDbTest : IDisposable
         // ASSERT
         result.ProjectId.Should().Be(proj1.Id);
         result.ProjectName.Should().Be(proj1.Name.ToString());
-        result.Members.
+        result.Members.Should().HaveCount(0);
+    }
+
+    [Fact]
+    public async Task GetProjectMembersFromDb_WhenProjectAndProjectMembersExists_ReturnResult()
+    {
+        // ARRANGE
+        Guid guid = _fixture.Create<Guid>();
+
+        var proj1 = _fixture.Create<Project>();
+        var proj2 = _fixture.Create<Project>();
+        _context.Projects.AddRange(proj1, proj2);
+
+        var user1 = _fixture.Create<User>(); //1
+        var user2 = _fixture.Create<User>(); //1 2
+        var user3 = _fixture.Create<User>(); //2
+        var user4 = _fixture.Create<User>(); // нигде
+
+        _context.Users.AddRange(user1, user2, user3, user4);
+
+        var member1 = ProjectMember.Create(
+            projectId: proj1.Id,
+            userId: user1.Id,
+            role: Domain.Entities.Enums.ProjectRoles.Admin,
+            joinedAt: _fixture.Create<DateTimeOffset>());
+
+        var member2 = ProjectMember.Create(
+            projectId: proj1.Id,
+            userId: user2.Id,
+            role: Domain.Entities.Enums.ProjectRoles.Viewer,
+            joinedAt: _fixture.Create<DateTimeOffset>());
+
+        var member3 = ProjectMember.Create(
+            projectId: proj2.Id,
+            userId: user2.Id,
+            role: Domain.Entities.Enums.ProjectRoles.Admin,
+            joinedAt: _fixture.Create<DateTimeOffset>());
+
+        var member4 = ProjectMember.Create(
+            projectId: proj2.Id,
+            userId: user3.Id,
+            role: Domain.Entities.Enums.ProjectRoles.Admin,
+            joinedAt: _fixture.Create<DateTimeOffset>());
+
+        _context.ProjectMembers.AddRange(member1,member2, member3, member4);
+
+        await _context.SaveChangesAsync(CancellationToken.None);
+        //ACT
+        var result = await _handler.GetProjectMembersFromDbAsync(proj1.Id, CancellationToken.None);
+
+        // ASSERT
+        result.ProjectId.Should().Be(proj1.Id);
+        result.ProjectName.Should().Be(proj1.Name.ToString());
+        result.Members.Should().HaveCount(2);
+
+        Guid[] guids = { user1.Id, user2.Id };
+        result.Members.Select(x => x.Id).Should().BeEquivalentTo(guids);
+
+        var memberRes1 = result.Members.First(x => x.Id == user1.Id);
+        memberRes1.KeycloakId.Should().Be(user1.KeycloakId);
+        memberRes1.Email.Should().Be(user1.Email.ToString());
+        memberRes1.UserName.Should().Be(user1.Username.ToString());
+        memberRes1.JoinedAt.Should().Be(member1.JoinedAt);
+        memberRes1.Role.Should().Be(member1.Role.ToDto());
+
+
+
+        var memberRes2 = result.Members.First(x => x.Id == user2.Id);
+        memberRes2.KeycloakId.Should().Be(user2.KeycloakId);
+        memberRes2.Email.Should().Be(user2.Email.ToString());
+        memberRes2.UserName.Should().Be(user2.Username.ToString());
+        memberRes2.JoinedAt.Should().Be(member2.JoinedAt);
+        memberRes2.Role.Should().Be(member2.Role.ToDto());
     }
 }
