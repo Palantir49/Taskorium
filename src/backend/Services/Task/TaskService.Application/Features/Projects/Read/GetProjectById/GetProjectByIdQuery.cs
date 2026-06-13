1288693d96a21bfc46b1f113ea5a53b710fceb55
jlamma <jlampjlamma@gmail.com>
2026-06-13 19:27:55 +0300

    [refacture] ProjectGetById handler and query

--- src/backend/Services/Task/TaskService.Application/Features/Projects/Read/GetProjectById/GetProjectByIdQuery.cs
+++ src/backend/Services/Task/TaskService.Application/Features/Projects/Read/GetProjectById/GetProjectByIdQuery.cs
@@ -3,4 +3,4 @@ using TaskService.Contracts.Project.Responses;
 
 namespace TaskService.Application.Features.Projects.Read.GetProjectById;
 
-public record class GetProjectByIdQuery(Guid? Id) : IQuery<ProjectResponse>;
+public record class GetProjectByIdQuery(Guid Id) : IQuery<ProjectResponse>;
