622fa0b9b5bcb231a17af8f35946815bddf988ad
jlamma <jlampjlamma@gmail.com>
2026-06-13 10:41:07 +0300

    [feature] added fake hybrid cache

---
+++ src/backend/Services/Task/TaskService.Tests/FakeHybridCache.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Caching.Hybrid;
+
+namespace TaskService.Tests;
+
+public class FakeHybridCache : HybridCache
+{
+    public override ValueTask<T> GetOrCreateAsync<TState, T>(string key, TState state, Func<TState, CancellationToken, ValueTask<T>> factory, HybridCacheEntryOptions? options = null, IEnumerable<string>? tags = null, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException("Кэш не должен вызываться в тестах бизнес-логики!");
+    }
+
+    public override ValueTask RemoveAsync(string key, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException("Кэш не должен вызываться в тестах бизнес-логики!");
+    }
+
+    public override ValueTask RemoveByTagAsync(string tag, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException("Кэш не должен вызываться в тестах бизнес-логики!");
+    }
+
+    public override ValueTask SetAsync<T>(string key, T value, HybridCacheEntryOptions? options = null, IEnumerable<string>? tags = null, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException("Кэш не должен вызываться в тестах бизнес-логики!");
+    }
+}
